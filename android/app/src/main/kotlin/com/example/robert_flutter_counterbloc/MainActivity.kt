5409c5c1820c2e2e76e8047efb54985539a0ddfb
Robert Perez <robertper280@gmail.com>
2025-02-10 00:56:45 -0800

    Uploading CounterBloc Assignment

---
+++ android/app/src/main/kotlin/com/example/robert_flutter_counterbloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.robert_flutter_counterbloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class InfoState {
+  int counter = 0;
+
+  InfoState(this.counter);
+}
+
+class InfoCubit extends Cubit<InfoState> {
+  InfoCubit() : super(InfoState(0));
+
+  void update() {
+    emit(InfoState(state.counter + 1));
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<InfoCubit>(
+      create: (context) => InfoCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: BlocBuilder<InfoCubit, InfoState>(
+          builder: (context, state) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    '${state.counter}',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
+              ),
+            );
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read<InfoCubit>().update(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
